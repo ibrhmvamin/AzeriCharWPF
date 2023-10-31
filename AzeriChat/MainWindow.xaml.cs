a3f1005073497b0fe7d6fae0bf4930b4106a9909
ibrhmvamin <aminibrahimov52@gmail.com>
2023-11-01 00:41:09 +0400

    AzeriChat

---
+++ AzeriChat/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace AzeriChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Message> messages { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            messages = new List<Message>() 
+            {
+            new Message("Salam",new DateTime(2023,10,30)) ,
+            new Message("Necesen",new DateTime(2023,10,31)) ,
+            new Message("5 ci gun futbol xaraso",new DateTime(2023,10,31)) ,
+            new Message("Barca Real",new DateTime(2023,10,31)) ,
+            new Message("Gedek ?",new DateTime(2023,10,31)) ,
+            };
+
+            DataContext = this;
+        }
+
+        private void EnterMe(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            accesslbl.Content = "writing...";
+            if (textbox.Text == "Tap for writing...") { 
+            textbox.Text = string.Empty;
+            }
+        }
+
+        private void LeaveMe(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            accesslbl.Content = "online";
+        }
+
+        private void SendClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textbox.Text)) 
+            {
+                Message message = new Message(textbox.Text,DateTime.Now);
+                messages.Add(message);
+                listview.Items.Add(message);
+                textbox.Text=string.Empty;
+            }
+        }
+
+        private void DoubleClickMe(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (textbox.Text == "Tap for writing...")
+            {
+                textbox.Text = string.Empty;
+                accesslbl.Content = "writing...";
+            }
+        }
+    }
+}
---
+++ AzeriChat/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzeriChat
+{
+    public class Message
+    {
+        public string? Text { get; set; }
+        public DateTime? Date { get; set; }
+        public Message() { }
+        public Message(string? text, DateTime? date)
+        {
+            Text = text;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{Text} - {Date}";
+        }
+    }
+}
